6b5a35e7305033cbda8aa6a2bf89ac24b7e75b50
Tyler Austin <tyler@webguyty.com>
2020-09-09 14:48:10 -0700

    Basic routes set up

--- client/src/index.js
+++ client/src/index.js
@@ -4,8 +4,8 @@ import App from './App';
 
 import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/solid';
-import '@fortawesome/fontawesome-free/js/regular';
-import '@fortawesome/fontawesome-free/js/brands';
+// import '@fortawesome/fontawesome-free/js/regular';
+// import '@fortawesome/fontawesome-free/js/brands';
 
 ReactDOM.render(
   <React.StrictMode>
